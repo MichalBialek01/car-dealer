b7e7d09f53a97274883a8cfed6a8deed22bbae9d
Michał Białek <kontakt.michalbialek@gmail.com>
2024-09-19 14:35:35 +0200

    feat: adding logback for test. Adding simple home controller WebMvcTest

---
+++ src/test/java/pl/bialek/api/controller/HomeControllerTest.java
@@ -0,0 +1,35 @@
+package pl.bialek.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(controllers = HomeController.class)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+/*
+onConstructor = @__(@Autowired) - mówi, że towrzony jest konstruktor, który jest oznaczony jako @Autowired
+ */
+class HomeControllerTest {
+    private MockMvc mockMvc;
+//    Symuluje wysyałanie requesta
+
+    @Test
+    void homeWorksCorrectly() throws Exception {
+//    GIVEN,WHEN,THEN
+        mockMvc.perform(get(HomeController.HOME))
+                .andExpect(status().isOk())
+                .andExpect(view().name("homepage"));
+
+//        Symulujemy wysłanie rquesta get na hontroler HomeController pod endpoint home
+//        I oczekiwaną wartością jest status ok, oraz zwracanie nazwy homepage
+
+
+    }
+
+}
