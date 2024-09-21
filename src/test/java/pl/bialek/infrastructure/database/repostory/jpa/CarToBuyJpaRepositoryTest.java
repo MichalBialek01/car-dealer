313251831c9cc54ed35e222192bc68fe41519b0f
Michał Białek <kontakt.michalbialek@gmail.com>
2024-09-21 13:56:06 +0200

    feat: Providing testcontainer configuration to DataJpaTest by PersistenceContainerTestConfiguration

--- src/test/java/pl/bialek/infrastructure/database/repostory/jpa/CarToBuyJpaRepositoryTest.java
+++ src/test/java/pl/bialek/infrastructure/database/repostory/jpa/CarToBuyJpaRepositoryTest.java
@@ -1,11 +1,14 @@
-package pl.bialek.api.infrastructure.database.repostory.jpa;
+package pl.bialek.infrastructure.database.repostory.jpa;
 
 import lombok.AllArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import pl.bialek.configuration.PersistenceContainerTestConfiguration;
 import pl.bialek.infrastructure.database.entity.CarToBuyEntity;
 import pl.bialek.infrastructure.database.repository.jpa.CarToBuyJpaRepository;
 
@@ -16,21 +19,22 @@ import static pl.bialek.util.EntityFixtures.car2;
 
 @DataJpaTest
 @TestPropertySource(locations = "classpath:application-test.yaml")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(PersistenceContainerTestConfiguration.class)
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CarToBuyJpaRepositoryTest {
 
     private CarToBuyJpaRepository carToBuyJpaRepository;
 
 
-//    TODO: Test not working properly because of H2 unattainable methods/syntax that is provided by Postgre -> hereafter implementing testcontainers
-//    @Test
-//    void thatCarCanBeSavedCorrectly() {
-////      Given
-//        var carsToBuy = List.of(car1(), car2());
-//        carToBuyJpaRepository.saveAllAndFlush(carsToBuy);
-////      When
-//        List<CarToBuyEntity> savedCars = carToBuyJpaRepository.findAvailableCars();
-////      Then
-//        Assertions.assertThat(savedCars).hasSize(8);
-//    }
+    @Test
+    void thatCarCanBeSavedCorrectly() {
+//      Given
+        var carsToBuy = List.of(car1(), car2());
+        carToBuyJpaRepository.saveAllAndFlush(carsToBuy);
+//      When
+        List<CarToBuyEntity> savedCars = carToBuyJpaRepository.findAvailableCars();
+//      Then
+        Assertions.assertThat(savedCars).hasSize(8);
+    }
 }
