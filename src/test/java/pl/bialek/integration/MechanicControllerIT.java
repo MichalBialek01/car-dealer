b2247d3320858848a7f965b74310137066522eec
Michał Białek <kontakt.michalbialek@gmail.com>
2024-09-21 14:33:59 +0200

    feat: Providing HomeControllerIT - issues with db connection, to further check

---
+++ src/test/java/pl/bialek/integration/MechanicControllerIT.java
@@ -0,0 +1,24 @@
+package pl.bialek.integration;
+
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import pl.bialek.configuration.AbstractIT;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class MechanicControllerIT extends AbstractIT {
+
+
+    private final TestRestTemplate testRestTemplate;
+
+    @Test
+    void mechanicPageWorksCorrectly() {
+        String url = String.format("http://localhost:%s%s", port, basePath);
+
+        String page = this.testRestTemplate.getForObject(url, String.class);
+        Assertions.assertThat(page).contains("service");
+    }
+
+}
