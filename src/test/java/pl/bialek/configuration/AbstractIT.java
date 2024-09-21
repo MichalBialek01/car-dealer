2ac7ff57c6d78f0c089e1e61bdd42a078370ee7d
Michał Białek <kontakt.michalbialek@gmail.com>
2024-09-21 14:28:48 +0200

    feat: Providing HomeControllerIT

---
+++ src/test/java/pl/bialek/configuration/AbstractIT.java
@@ -0,0 +1,26 @@
+package pl.bialek.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import pl.bialek.CarDealershipApplication;
+
+
+@ActiveProfiles("test")
+@Import(PersistenceContainerTestConfiguration.class)
+@SpringBootTest(
+        classes = CarDealershipApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+public abstract class AbstractIT {
+
+    @LocalServerPort
+    protected int port;
+
+    @Value("${server.servlet.context-path}")
+    protected String basePath;
+
+
+}
